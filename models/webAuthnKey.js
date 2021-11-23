828dcd44c85447c4d9845ba3565896940a406a67
Vovk Viktor <viktor.vovk@vilmate.com>
2021-11-23 16:13:53 +0200

    add date Create

--- models/webAuthnKey.js
+++ models/webAuthnKey.js
@@ -1,16 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const AuthnKey = mongoose.model('Authnkey', new mongoose.Schema({
-  userId: {
-    type: mongoose.ObjectId,
-    required: true
-  },
-  key: {
-    type: Object,
-    required: true
-  }
-}))
+const AuthnKey = mongoose.model(
+  "Authnkey",
+  new mongoose.Schema({
+    userId: {
+      type: mongoose.ObjectId,
+      required: true,
+    },
+    key: {
+      type: Object,
+      required: true,
+    },
+    dataCreate: {
+      type: Date,
+      default: new Date(),
+    },
+  })
+);
 
 module.exports = {
-  AuthnKey
-}
+  AuthnKey,
+};
