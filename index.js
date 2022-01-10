fc94242f60578bfe3943c8571eb8dbb07d6a4dc2
Vovk Viktor <viktor.vovk@vilmate.com>
2022-01-10 14:18:12 +0200

    check

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(
     keys: [crypto.randomBytes(32).toString("hex")],
     // Cookie Options
     maxAge: 24 * 60 * 60 * 1000, // 24 hours
-    sameSite: false,
+    sameSite: "none",
     secure: false,
   })
 );
